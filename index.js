ead2f4d76d12c1be8229725d031d24a6dc7c7d86
MutaiAllan <MutaiAllan>
2023-07-08 20:20:07 +0300

    Added fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body:JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+    .then(res => res.json())
+    .then(function (object) {
+    var page = document.querySelector('body')
+    const li = document.createElement('li')
+    page.append(li)
+    const ul = document.createElement('ul')
+    ul.innerText = object
+    li.appendChild(ul)
+    })
+    .catch(function (error) {
+        console.log(error.message);
+    })
+}
